3ead59bcf4084f3f5c9f8d46d13c53c28e1c8d1b
Oksana Moroz <k.moroz.mail@gmail.com>
2023-10-05 15:59:46 +0300

    added category

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { ReactSortable } from "react-sortablejs";
+import Spinner from "./Spinner";
 
 export default function ProductForm({
   _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImages,
+  category: assignedCategory,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
+  const [isUpLoading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
   const router = useRouter();
 
   async function saveProduct(ev) {
@@ -20,12 +28,13 @@ export default function ProductForm({
       title,
       description,
       price,
+      images,
+      category,
     };
+
     if (_id) {
-      //update
       await axios.put("/api/products", { ...data, _id });
     } else {
-      //create
       await axios.post("/api/products", data);
     }
     setGoToProducts(true);
@@ -33,6 +42,29 @@ export default function ProductForm({
   if (goToProducts) {
     router.push("/products");
   }
+
+  async function uploadImages(ev) {
+    console.log(ev);
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+      console.log(res.data);
+      setIsUploading(false);
+    }
+  }
+
+  function updateImagesOrder(images) {
+    setImages(images);
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -42,12 +74,56 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Category</label>
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-1">
+        <ReactSortable
+          list={images}
+          className="flex flex-wrap gap-1"
+          setList={updateImagesOrder}
+        >
+          {!!images?.length &&
+            images.map((link) => (
+              <div
+                key={link}
+                className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200"
+              >
+                <img src={link} alt="" className="rounded-lg" />
+              </div>
+            ))}
+        </ReactSortable>
+
+        {isUpLoading && (
+          <div className="h-24 flex items-center">
+            <Spinner />
+          </div>
+        )}
+
+        <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-grey-500 rounded-sm shadow-sm bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Add image</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
         value={description}
         onChange={(ev) => setDescription(ev.target.value)}
-      ></textarea>
+      />
       <label>Price (in USD)</label>
       <input
         type="number"
