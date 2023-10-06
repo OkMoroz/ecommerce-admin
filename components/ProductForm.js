972884cc654941b901a82dfeb84e5b500c441dcb
Oksana Moroz <k.moroz.mail@gmail.com>
2023-10-06 23:27:35 +0300

    added category

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -23,6 +23,12 @@ export default function ProductForm({
 
   const router = useRouter();
 
+  useEffect(() => {
+    axios.get('/api/categories').then(result => {
+      setCategories(result.data);
+    })
+  }, []);
+
   async function saveProduct(ev) {
     ev.preventDefault();
     const data = {
@@ -75,6 +81,17 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Category</label>
+      <select value={category}
+      onChange={ev=>setCategory(ev.target.value)}>
+        <option value="">Uncategorized</option>
+        {categories.length > 0 &&
+          categories.map((c) => (
+            <option key={c._id} value={c._id}>
+              {c.name}
+            </option>
+          ))}
+      </select>
       <label>Photos</label>
       <div className="mb-2 flex flex-wrap gap-1">
         <ReactSortable
--- pages/api/products.js
+++ pages/api/products.js
@@ -14,22 +14,26 @@ export default async function handle(req, res) {
   }
 
   if (method === "POST") {
-    const { title, description, price, images } = req.body;
+    const { title, description, price, images, category } = req.body;
     const productDoc = await Product.create({
       title,
       description,
       price,
       images,
+      category,
     });
     res.json(productDoc);
   }
 
-    if (method === "PUT") {
-      const { title, description, price, images, _id } = req.body;
-     await Product.updateOne({ _id }, { title, description, price, images });
-      res.json(true);
+  if (method === "PUT") {
+    const { title, description, price, images, category, _id } = req.body;
+    await Product.updateOne(
+      { _id },
+      { title, description, price, images, category }
+    );
+    res.json(true);
   }
-  
+
   if (method === "DELETE") {
     if (req.query?.id) {
       await Product.deleteOne({ _id: req.query?.id });
@@ -37,4 +41,3 @@ export default async function handle(req, res) {
     }
   }
 }
-
